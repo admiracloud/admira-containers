71c6f4c124f45ba1e639732c7723167f5ceeca9b
Paulo Coghi <paulo@adimira.com>
2023-08-08 20:34:59 +0200

    Check if container with same name already exists

--- src/cli/commands.cr
+++ src/cli/commands.cr
@@ -19,14 +19,27 @@ struct Commands
     # Exit if invalid
     @validate.create(args)
 
-    # Otherwise
+    # Proactively check if the container already exists
+    if @admiractl.exists(args[0])
+      puts "Container #{args[0]} already exists"
+      exit
+    end
+
+    # Otherwise, start the creation
+    # avoiding to check again if a container with the
+    # same name already exists, using "false" on the
+    # seccond argument of @admiractl.create method
     puts "Creating container #{args[0]}..."
-    if @admiractl.create(args)
-      # In case of success
+    result = @admiractl.create(args, false)
+
+    # Print the result to the user
+    case result
+    when 1
       puts "Container #{args[0]} created successfully"
     else
       puts "An error occurred while attempting to create the container #{args[0]}"
     end
+
     exit
   end
 
--- src/library/admiractl.cr
+++ src/library/admiractl.cr
@@ -1,18 +1,41 @@
+# helpers with methods to to the heavy lifting,
+# like transforming a raw command output into an
+# array of container instances
 require "./helpers/helpers.cr"
 
 struct Admiractl
   # Helpers methods
   @helpers = Helpers.new
 
-  def create(args : Array(String))
-    # TODO
-    # first check if container already exists, using list command
+  # return codes
+  # 1  : container created
+  # 0  : container already exists
+  # -1 : external error
+  def create(args : Array(String), check_exists : Bool = true) : Int32
+    if check_exists & exists(args[0])
+      return 0
+    end
+
     `lxc-create -n #{args[0]} --template debian-12-download --quiet`
-    return $?.success?
+    return $?.success? ? 1 : -1
   end
 
   def list
     raw = `lxc-ls -f -F NAME,STATE,PID,RAM,SWAP,AUTOSTART,GROUPS,INTERFACE,IPV4,IPV6,UNPRIVILEGED`
     return @helpers.container_list(raw)
+  end
+
+  def exists(name : String)
+    exists : Bool = false
+
+    container_list = list
+    container_list.each do |container|
+      if container.name == name
+        exists = true
+        break
+      end
+    end
+
+    return exists
   end
 end
